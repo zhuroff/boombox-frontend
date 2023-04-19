c79a9c7bfa1d880bce22795f877e0ae30f6f1e73
zhuroff <ezhuroff@gmail.com>
2023-04-19 23:45:23 +0300

    masonry for index page random albums

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,9 +12,17 @@
       </div>
 
       <div v-if="albums.isFetched" key="events">
-        <ul>
-          <li v-for="item in albums.data" :key="item._id">
-            <img :src="item.albumCover" alt="Well">
+        <ul class="masonry">
+          <li v-for="album in albums.data" :key="album._id" class="masonry__item">
+            <router-link class="masonry__link" :to="{ path: `/albums/${album._id}` }">
+              <img class="masonry__image" :src="album.albumCover" :alt="album.title">
+              <div class="masonry__caption">
+                <div class="masonry__caption-heading">{{ album.title }}</div>
+                <div class="masonry__caption-description">
+                  by {{ album.artist.title }}, {{ album.period.title }}. {{ album.genre.title }}
+                </div>
+              </div>
+            </router-link>
           </li>
         </ul>
       </div>
@@ -72,4 +80,138 @@ export default defineComponent({
 <style lang="scss" scoped>
 @import '~/scss/variables';
 @import 'include-media';
+
+.masonry {
+  display: grid;
+  grid-template-columns: 50% 25% 12.5% 12.5%;
+
+  &__item {
+
+    &:nth-child(1) {
+      grid-row: 1 / span 4;
+      font-size: 300%;
+    }
+
+    &:nth-child(2) {
+      grid-row: 1 / span 2;
+      font-size: 175%;
+    }
+
+    &:nth-child(3) {
+      grid-row: 3 / span 2;
+      font-size: 175%;
+    }
+
+    &:nth-child(n+4) {
+      font-size: 125%;
+    }
+
+    &:nth-child(4) {
+      grid-row: 1;
+      grid-column: 3;
+    }
+
+    &:nth-child(5) {
+      grid-row: 1;
+      grid-column: 4;
+    }
+
+    &:nth-child(6) {
+      grid-row: 2;
+      grid-column: 3;
+    }
+
+    &:nth-child(7) {
+      grid-row: 2;
+      grid-column: 4;
+    }
+
+    &:nth-child(8) {
+      grid-row: 3;
+      grid-column: 3;
+    }
+
+    &:nth-child(9) {
+      grid-row: 3;
+      grid-column: 4;
+    }
+
+    &:nth-child(10) {
+      grid-row: 4;
+      grid-column: 3;
+    }
+
+    &:nth-child(11) {
+      grid-row: 4;
+      grid-column: 4;
+    }
+
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  &__link {
+    position: relative;
+    overflow: hidden;
+    display: block;
+
+    &:hover {
+      .masonry__caption {
+        opacity: 1;
+        transform: translateY(0);
+        transition: all 0.5s $animation;
+
+        &-heading {
+          opacity: 1;
+          transform: translateY(0);
+          transition: all 0.75s $animation;
+        }
+
+        &-description {
+          opacity: 1;
+          transform: translateY(0);
+          transition: all 0.75s $animation;
+        }
+      }
+    }
+  }
+
+  &__caption {
+    position: absolute;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    bottom: 0;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 0 5% 5%;
+    opacity: 0;
+    transform: translateY(25%);
+    transition: all 0.5s $animation;
+    background: linear-gradient(to top, $dark 25%, transparent 100%);
+
+    &-heading {
+      font-weight: 600;
+      color: $white;
+      opacity: 0;
+      transform: translateY(50%);
+      transition: all 0.75s $animation;
+    }
+
+    &-description {
+      font-size: 70%;
+      color: $border;
+      opacity: 0;
+      transform: translateY(10%);
+      transition: all 0.75s $animation;
+    }
+  }
+}
 </style>
--- src/services/IndexServices.ts
+++ src/services/IndexServices.ts
@@ -1,7 +1,7 @@
 import api from '~/api'
 
 export default class AlbumServices {
-  static async getRandomAlbums(quantity = 8) {
+  static async getRandomAlbums(quantity = 11) {
     const response = await api.get<any>('/api/albums', { params: { quantity } })
 
     if (response?.status === 200) {
